cc9703e1b2694e4edd62e0d6264b492d55107bba
Anylisse <121113301+Anylisse@users.noreply.github.com>
2023-06-15 17:22:15 +0200

    Installation de MongoDB et connexion réussie

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://anylisse:PasswordmongoDB@atlascluster.2iinmf8.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
 module.exports = app;
