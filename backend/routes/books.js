e6fbaa49552c16dc46ba94a346a95c43edf3b01a
Anylisse <121113301+Anylisse@users.noreply.github.com>
2023-06-20 16:30:37 +0200

    Création du dossier controllers avec fichier books.js

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const booksCtrl = require('../controllers/books');
+
 router.get('/', booksCtrl.getAllBooks);
 router.get('/bestrating', booksCtrl.getBestRatingBook);
 router.get('/:id', booksCtrl.getOneBook);
-router.post('/', booksCtrl.createBook);
 router.post('/:id/rating', booksCtrl.ratingBook);
+router.post('/', booksCtrl.createBook);
 router.put('/:id', booksCtrl.modifyBook);
 router.delete(':id', booksCtrl.deleteBook);
 
