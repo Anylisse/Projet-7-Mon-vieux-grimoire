9d456aebfd7a3d7f120745cf642cb2f7aba8c1df
Anylisse <121113301+Anylisse@users.noreply.github.com>
2023-06-21 10:02:47 +0200

    Création du dossier middleware avec fichier auth.js et installation de multer

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const booksCtrl = require('../controllers/books');
 
 router.get('/', booksCtrl.getAllBooks);
 router.get('/bestrating', booksCtrl.getBestRatingBook);
 router.get('/:id', booksCtrl.getOneBook);
-router.post('/:id/rating', booksCtrl.ratingBook);
-router.post('/', booksCtrl.createBook);
-router.put('/:id', booksCtrl.modifyBook);
-router.delete(':id', booksCtrl.deleteBook);
+router.post('/:id/rating', auth, booksCtrl.ratingBook);
+router.post('/', auth, booksCtrl.createBook);
+router.put('/:id', auth, booksCtrl.modifyBook);
+router.delete(':id', auth, booksCtrl.deleteBook);
 
 module.exports = router;
