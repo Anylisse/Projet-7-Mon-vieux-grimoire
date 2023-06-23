52ef6be66667dc026d59a1b3f6027a883128d465
Anylisse <121113301+Anylisse@users.noreply.github.com>
2023-06-23 13:12:47 +0200

    Création fichier cors.js dans dossier middleware et installation dotenv

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 
+require('dotenv').config();
+
 const booksRoutes = require('./routes/books');
 const userRoutes = require('./routes/user');
 
+const { cors } = require('./middleware/cors');
+
 const app = express();
 
-mongoose.connect('mongodb+srv://anylisse:PasswordmongoDB@atlascluster.2iinmf8.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -15,14 +19,9 @@ mongoose.connect('mongodb+srv://anylisse:PasswordmongoDB@atlascluster.2iinmf8.mo
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
-
 app.use(express.json());
+
+app.use(cors);
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/books', booksRoutes);
